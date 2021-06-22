cc49c2f18039a35337df97e63e8e571d0244a585
Minxyzgo <1225327866@qq.com>
2021-06-22 21:29:46 +0800

    update

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,3 +1,7 @@
+import javax.script.ScriptEngineManager
+
 fun main() {
-   // Compiler.compile()
+   val classLoader = Thread.currentThread().contextClassLoader
+   val engineManager = ScriptEngineManager(classLoader)
+   engineManager.getEngineByExtension("kts").eval("val o = 0")
 }
